3dae3dd983a97de40d36410badbf67560a094f97
Gokhan Simsek <g.simsek@paralect.com>
2020-09-16 09:37:11 +0300

    Adding: Object detection using HSV color space from video

--- Object-Detection-Using-HSV-Color-Space/detect-video.py
+++ Object-Detection-Using-HSV-Color-Space/detect-video.py
@@ -3,8 +3,8 @@ import numpy as np
 
 def noop(x): return None
 
-## Object Detection Using HSV Color Space
-## HSV (Hue, Saturation, Value)
+#### Object detection from video using HSV color space
+#### (HSV -> Hue, Saturation, Value)
 
 ## 1. Create "Set Mask" window with default HSV range to detect blue color
 SET_MASK_WINDOW = "Set Mask"
@@ -16,13 +16,17 @@ cv2.createTrackbar("Max Sat", SET_MASK_WINDOW, 255, 255, noop)
 cv2.createTrackbar("Min Val", SET_MASK_WINDOW, 0, 255, noop)
 cv2.createTrackbar("Max Val", SET_MASK_WINDOW, 255, 255, noop)
 
-while True:
-    ## 2. Read and convert image to HSV color space
-    image = cv2.imread('smarties.png')
-    imageHsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    # https://github.com/opencv/opencv/blob/master/samples/data/smarties.png
+## 2. Capture from default camera
+videoCapture = cv2.VideoCapture(0)
 
-    ## 3. Get min and max HSV values from Set Mask window
+while True:
+    ## 3. Capture video from camera and convert to HSV color space
+    _, capturedVideo = videoCapture.read() # video
+    # rotate video 180 degrees (it starts upside down on Windows)
+    capturedVideo = cv2.flip(capturedVideo, -1)
+    capturedVideoHsv = cv2.cvtColor(capturedVideo, cv2.COLOR_BGR2HSV)
+
+    ## 4. Get min and max HSV values from Set Mask window
     minHue = cv2.getTrackbarPos("Min Hue", SET_MASK_WINDOW)
     maxHue = cv2.getTrackbarPos("Max Hue", SET_MASK_WINDOW)
     minSat = cv2.getTrackbarPos("Min Sat", SET_MASK_WINDOW)
@@ -32,15 +36,17 @@ while True:
     minHsv = np.array([minHue, minSat, minVal])
     maxHsv = np.array([maxHue, maxSat, maxVal])
 
-    # 4. Create mask and result (masked) image
-    mask = cv2.inRange(imageHsv, minHsv, maxHsv)
-    resultImage = cv2.bitwise_and(image, image, mask=mask)
+    ## 5. Create mask and result (masked) video
+    mask = cv2.inRange(capturedVideoHsv, minHsv, maxHsv)
+    resultVideo = cv2.bitwise_and(capturedVideo, capturedVideo, mask=mask)
 
-    # 5. Show images
-    cv2.imshow("Input Image", image)
+    ## 6. Show videos
+    cv2.imshow("Captured Video", capturedVideo)
     # cv2.imshow("Mask", mask)
-    cv2.imshow("Result Image", resultImage)
-    cv2.waitKey(1)
+    cv2.imshow("Result (Masked) Video", resultVideo)
+    # ---
+    key = cv2.waitKey(1)
+    if key == 27: break
 
+videoCapture.release()
 cv2.destroyAllWindows()
-
