9664dcd7b08c37cd07b8a0c6c47845eaac5b0625
Gokhan Simsek <g.simsek@paralect.com>
2020-09-18 10:30:11 +0300

    Adding Image Similarity Search code

---
+++ Image-Similarity-Search/1.js
@@ -0,0 +1,11 @@
+"mllib":"caffe" // Caffe is a deep learning framework developed by Berkeley AI Research (BAIR) 
+
+"mllib":{
+  "nclasses": 1000,		        // Number of classes of images this model can recognize
+  "template": "se_resnet_50"  // Use ResNet50 deep convolutional neural network model
+}
+
+"mllib":{ 
+  "extract_layer": "pool5/7x7_s1"  // Use “pool5/7x7_s1” layer of ResNet50 model
+					                         //  (pooling layer, kernel size: 7, stride: 1)	
+}
