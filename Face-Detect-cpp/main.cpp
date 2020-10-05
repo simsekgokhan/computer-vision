83cef33f6a5253d1d9464fdf91987cc8fea29851
Gokhan Simsek <g.simsek@paralect.com>
2020-10-05 17:47:47 +0300

    Adding Face-Detect-cpp

---
+++ Face-Detect-cpp/main.cpp
@@ -0,0 +1,15 @@
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+
+#include <iostream>
+
+int main() 
+{
+    std::cout << "---- Start ---- \n";
+
+    std::cout << " bbbbb \n";
+
+    std::cout << "---- End ---- \n";
+}
+
