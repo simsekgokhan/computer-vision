c4855379180a0c36715b6d993844bd0075b9c8f8
Gokhan Simsek <g.simsek@paralect.com>
2020-10-17 13:53:17 +0300

    Minor refactoring

--- Object-Detection-Using-HSV-Color-Space/detect-image.py
+++ Object-Detection-Using-HSV-Color-Space/detect-image.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
 def noop(x): return None
@@ -8,39 +8,37 @@ def noop(x): return None
 
 ## 1. Create "Set Mask" window with default HSV range to detect blue color
 SET_MASK_WINDOW = "Set Mask"
-cv2.namedWindow(SET_MASK_WINDOW, cv2.WINDOW_NORMAL)
-cv2.createTrackbar("Min Hue", SET_MASK_WINDOW, 90, 179, noop)
-cv2.createTrackbar("Max Hue", SET_MASK_WINDOW, 140, 179, noop)
-cv2.createTrackbar("Min Sat", SET_MASK_WINDOW, 74, 255, noop)
-cv2.createTrackbar("Max Sat", SET_MASK_WINDOW, 255, 255, noop)
-cv2.createTrackbar("Min Val", SET_MASK_WINDOW, 0, 255, noop)
-cv2.createTrackbar("Max Val", SET_MASK_WINDOW, 255, 255, noop)
+cv.namedWindow(SET_MASK_WINDOW, cv.WINDOW_NORMAL)
+cv.createTrackbar("Min Hue", SET_MASK_WINDOW, 90, 179, noop)
+cv.createTrackbar("Max Hue", SET_MASK_WINDOW, 140, 179, noop)
+cv.createTrackbar("Min Sat", SET_MASK_WINDOW, 74, 255, noop)
+cv.createTrackbar("Max Sat", SET_MASK_WINDOW, 255, 255, noop)
+cv.createTrackbar("Min Val", SET_MASK_WINDOW, 0, 255, noop)
+cv.createTrackbar("Max Val", SET_MASK_WINDOW, 255, 255, noop)
 
 while True:
     ## 2. Read and convert image to HSV color space
-    image = cv2.imread('paralect.png')
-    imageHsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    image = cv.imread('paralect.png')
+    imageHsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
 
     ## 3. Get min and max HSV values from Set Mask window
-    minHue = cv2.getTrackbarPos("Min Hue", SET_MASK_WINDOW)
-    maxHue = cv2.getTrackbarPos("Max Hue", SET_MASK_WINDOW)
-    minSat = cv2.getTrackbarPos("Min Sat", SET_MASK_WINDOW)
-    maxSat = cv2.getTrackbarPos("Max Sat", SET_MASK_WINDOW)
-    minVal = cv2.getTrackbarPos("Min Val", SET_MASK_WINDOW)
-    maxVal = cv2.getTrackbarPos("Max Val", SET_MASK_WINDOW)
+    minHue = cv.getTrackbarPos("Min Hue", SET_MASK_WINDOW)
+    maxHue = cv.getTrackbarPos("Max Hue", SET_MASK_WINDOW)
+    minSat = cv.getTrackbarPos("Min Sat", SET_MASK_WINDOW)
+    maxSat = cv.getTrackbarPos("Max Sat", SET_MASK_WINDOW)
+    minVal = cv.getTrackbarPos("Min Val", SET_MASK_WINDOW)
+    maxVal = cv.getTrackbarPos("Max Val", SET_MASK_WINDOW)
     minHsv = np.array([minHue, minSat, minVal])
     maxHsv = np.array([maxHue, maxSat, maxVal])
 
     ## 4. Create mask and result (masked) image
-    mask = cv2.inRange(imageHsv, minHsv, maxHsv)
-    resultImage = cv2.bitwise_and(image, image, mask=mask)
+    # params: input array, lower boundary array, upper boundary array
+    mask = cv.inRange(imageHsv, minHsv, maxHsv)
+    # params: src1	array, src2 array, mask
+    resultImage = cv.bitwise_and(image, image, mask=mask)
 
     ## 5. Show images
-    cv2.imshow("Input Image", image)
-    # cv2.imshow("Mask", mask)
-    cv2.imshow("Result Image", resultImage)
-    # ---
-    key = cv2.waitKey(1)
-    if key == 27: break
-
-cv2.destroyAllWindows()
+    cv.imshow("Input Image", image)
+    # cv.imshow("Mask", mask) // optional
+    cv.imshow("Result Image", resultImage)
+    if cv.waitKey(1) == 27: break   # Wait Esc key to end program
--- Object-Detection-Using-HSV-Color-Space/detect-video.py
+++ Object-Detection-Using-HSV-Color-Space/detect-video.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
 def noop(x): return None
@@ -8,46 +8,43 @@ def noop(x): return None
 
 ## 1. Create "Set Mask" window with default HSV range to detect blue color
 SET_MASK_WINDOW = "Set Mask"
-cv2.namedWindow(SET_MASK_WINDOW, cv2.WINDOW_NORMAL)
-cv2.createTrackbar("Min Hue", SET_MASK_WINDOW, 90, 179, noop)
-cv2.createTrackbar("Max Hue", SET_MASK_WINDOW, 140, 179, noop)
-cv2.createTrackbar("Min Sat", SET_MASK_WINDOW, 74, 255, noop)
-cv2.createTrackbar("Max Sat", SET_MASK_WINDOW, 255, 255, noop)
-cv2.createTrackbar("Min Val", SET_MASK_WINDOW, 0, 255, noop)
-cv2.createTrackbar("Max Val", SET_MASK_WINDOW, 255, 255, noop)
+cv.namedWindow(SET_MASK_WINDOW, cv.WINDOW_NORMAL)
+cv.createTrackbar("Min Hue", SET_MASK_WINDOW, 90, 179, noop)
+cv.createTrackbar("Max Hue", SET_MASK_WINDOW, 140, 179, noop)
+cv.createTrackbar("Min Sat", SET_MASK_WINDOW, 74, 255, noop)
+cv.createTrackbar("Max Sat", SET_MASK_WINDOW, 255, 255, noop)
+cv.createTrackbar("Min Val", SET_MASK_WINDOW, 0, 255, noop)
+cv.createTrackbar("Max Val", SET_MASK_WINDOW, 255, 255, noop)
 
 ## 2. Capture from default camera
-videoCapture = cv2.VideoCapture(0)
+videoCapture = cv.VideoCapture(0)
 
 while True:
     ## 3. Capture video from camera and convert to HSV color space
     _, capturedVideo = videoCapture.read() # video
     # rotate video 180 degrees (it starts upside down on Windows)
-    # capturedVideo = cv2.flip(capturedVideo, 1) # Win                                
-    capturedVideo = cv2.resize(capturedVideo, None, None, fx=0.5, fy=0.5) # macOS
-    capturedVideoHsv = cv2.cvtColor(capturedVideo, cv2.COLOR_BGR2HSV)
+    # capturedVideo = cv.flip(capturedVideo, 1) # Win                                
+    capturedVideo = cv.resize(capturedVideo, None, None, fx=0.5, fy=0.5) # macOS
+    capturedVideoHsv = cv.cvtColor(capturedVideo, cv.COLOR_BGR2HSV)
 
     ## 4. Get min and max HSV values from Set Mask window
-    minHue = cv2.getTrackbarPos("Min Hue", SET_MASK_WINDOW)
-    maxHue = cv2.getTrackbarPos("Max Hue", SET_MASK_WINDOW)
-    minSat = cv2.getTrackbarPos("Min Sat", SET_MASK_WINDOW)
-    maxSat = cv2.getTrackbarPos("Max Sat", SET_MASK_WINDOW)
-    minVal = cv2.getTrackbarPos("Min Val", SET_MASK_WINDOW)
-    maxVal = cv2.getTrackbarPos("Max Val", SET_MASK_WINDOW)
+    minHue = cv.getTrackbarPos("Min Hue", SET_MASK_WINDOW)
+    maxHue = cv.getTrackbarPos("Max Hue", SET_MASK_WINDOW)
+    minSat = cv.getTrackbarPos("Min Sat", SET_MASK_WINDOW)
+    maxSat = cv.getTrackbarPos("Max Sat", SET_MASK_WINDOW)
+    minVal = cv.getTrackbarPos("Min Val", SET_MASK_WINDOW)
+    maxVal = cv.getTrackbarPos("Max Val", SET_MASK_WINDOW)
     minHsv = np.array([minHue, minSat, minVal])
     maxHsv = np.array([maxHue, maxSat, maxVal])
 
     ## 5. Create mask and result (masked) video
-    mask = cv2.inRange(capturedVideoHsv, minHsv, maxHsv)
-    resultVideo = cv2.bitwise_and(capturedVideo, capturedVideo, mask=mask)
+    # params: input array, lower boundary array, upper boundary array
+    mask = cv.inRange(capturedVideoHsv, minHsv, maxHsv)
+    # params: src1	array, src2 array, mask
+    resultVideo = cv.bitwise_and(capturedVideo, capturedVideo, mask=mask)
 
     ## 6. Show videos
-    cv2.imshow("Captured Video", capturedVideo)
-    # cv2.imshow("Mask", mask)
-    cv2.imshow("Result (Masked) Video", resultVideo)
-    # ---
-    key = cv2.waitKey(1)
-    if key == 27: break
-
-videoCapture.release()
-cv2.destroyAllWindows()
+    cv.imshow("Captured Video", capturedVideo)
+    # cv.imshow("Mask", mask)  // optional
+    cv.imshow("Result (Masked) Video", resultVideo)
+    if cv.waitKey(1) == 27: break   # Wait Esc key to end program
